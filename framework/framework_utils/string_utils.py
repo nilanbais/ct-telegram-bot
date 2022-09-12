ee742a8d7b88eafad7bb736d5b656ba2d7de138f
Bais <32563996+nilanbais@users.noreply.github.com>
2022-09-12 10:34:02 +0200

    Update string_utils.py
    Implementation of case functionality

--- framework/framework_utils/string_utils.py
+++ framework/framework_utils/string_utils.py
@@ -22,3 +22,21 @@ def var_name_from_name_str(name_sting: str, usage: str) -> str:
         return 'API_BEARER_TOKEN_' + __string
     elif usage == 'endpoints':
         return __string
+
+def var_name_from_name_string(name_string: str, usage: str) -> str:
+    __string = str.upper(name_string)
+    __string = __string.replace("-", "_").replace(" ", "_")
+    match usage:
+        case 'api-key':
+            return "API_KEY_" + __string
+        case 'bearer-token':
+            return 'API_BEARER_TOKEN_' + __string
+        case 'endpoints':
+            return __string
+
+        
+def check_for_char(input_string: str, character: str) -> bool:
+    """Function to check if a character is present in the given string.
+        Returns a boolean value.
+    """
+    return character in input_string
