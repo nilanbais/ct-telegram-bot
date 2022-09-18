e9f930959b1d08485c828edd896849e2bb934a04
Bais <32563996+nilanbais@users.noreply.github.com>
2022-09-18 21:03:40 +0200

    Cleanup

--- framework/database/database_cursor.py
+++ framework/database/database_cursor.py
@@ -1,36 +1,6 @@
 from typing import List
-from psycopg2.errors import DuplicateTable
 
-from pymongo.database import Database, Collection
-
-from framework.database import DatabaseConnection, MongoDBConnection
-
-
-class DatabaseCursor:
-
-    def __init__(self, connector_object: DatabaseConnection) -> None:
-        self.connector = connector_object
-
-    def query_db(self, query_string: str):
-        """Method to standardise the process of querying a database.
-            Returns the whole result of the query.
-        """
-        with self.connector.connect as conn:
-            cursor = conn.cursor()
-            cursor.execute(query=query_string)
-            result = cursor.fetchall()
-        
-        return result
-    
-    def create(self, query_string: str) -> None:
-        """
-        """
-        with self.connector.connect as conn:
-            cursor = conn.cursor()
-            try:
-                cursor.execute(query=query_string)
-            except DuplicateTable as error:
-                print(f"Skipping query. {str.capitalize(error)}")
+from framework.database import MongoDBConnection
 
 
 class MongoDBCursor:
